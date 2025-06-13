ea9a727adb3a546d7329991824b7229526eab7ed
Jonathan <dos.santos.2k23@gmail.com>
2025-06-13 13:56:17 -0300

    Feature: Specifying project colors

---
+++ forte_distribuidora/lib/utils/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+//color
+const kPrimaryColor = Color(0xff0379CC);
+const kSecondaryColor = Color(0xff5CB2F2);
+const kDarkblueColor = Color(0xff25396f);
+const kgreyColor = Color(0xfff1f1f1);
+const kDarkgreyColor = Color(0xff8d91a7);
+const kWhiteColor = Color(0xffffffff);
+
+const kDefaultPadding = 20.0;
+const kMaxWidth = 1232.0;
+const kDefaultDuration = Duration(milliseconds: 250);
+
+//our product shadow
+final kDefaultShadow = BoxShadow(
+  offset: const Offset(0, 10),
+  spreadRadius: 5,
+  blurRadius: 20,
+  color: const Color(0xFF0700B1).withAlpha(38),
+);
