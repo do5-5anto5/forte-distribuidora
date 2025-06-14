9b4bf66938fba60eef5664fa66dbfacb98da3c6b
Jonathan <dos.santos.2k23@gmail.com>
2025-06-14 18:30:16 -0300

    refactor: constants' imports packages, and wrap home elements in SingleScrollView

--- forte_distribuidora/lib/screens/home/home_screen.dart
+++ forte_distribuidora/lib/screens/home/home_screen.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:forte_distribuidora/screens/widgets/home/hero_banner.dart';
 
+import '../widgets/home/value_proposition.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const HeroBanner();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const HeroBanner(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: const ValueProposition(),
+          ),
+        ],
+      ),
+    );
   }
 }
