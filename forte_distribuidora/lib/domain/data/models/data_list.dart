7431b83e06b8ccc757f2deb18ced5e4b9d6059bd
Jonathan <dos.santos.2k23@gmail.com>
2025-06-16 18:02:29 -0300

    Feature: Add product's categories, and filtered lists by category

--- forte_distribuidora/lib/domain/data/models/data_list.dart
+++ forte_distribuidora/lib/domain/data/models/data_list.dart
@@ -1,40 +1,64 @@
 import 'package:forte_distribuidora/domain/data/models/product.dart';
 
-List<Product> products = [
+List<Product> allProducts = [
   Product(
     name: 'Placa de Drywall',
     image: 'assets/images/drywall_boards.png',
     description: 'Placa de gesso acartonado para paredes e forros.',
-    category: 'Placa'
+    category: Category.sheet.name
   ),
   Product(
     name: 'Placa Glasroc',
     image: 'assets/images/glasroc_boards.png',
     description: 'Placa de gesso com véu de vidro, ultra-resistente à umidade, mofo, UV e fogo.',
-    category: 'Placa'
+    category: Category.sheet.name
   ),
   Product(
     name: 'Perfil F530',
     image: 'assets/images/f530.png',
     description: 'Canaleta de aço galvanizado para a estrutura de forros de drywall.',
-    category: 'Ferragem'
+    category: Category.metalFraming.name
   ),
   Product(
     name: 'Fita Telada',
     image: 'assets/images/fita_telada.png',
     description: 'Tela de fibra autoadesiva para juntas de drywall.',
-    category: 'Fita'
+    category: Category.tape.name
   ),
   Product(
     name: 'GN25',
     image: 'assets/images/gn25.png',
     description: 'Autoperfurante, cabeça trombeta e ponta agulha.',
-    category: 'Parafuso'
+    category: Category.screws.name
   ),
   Product(
     name: 'Massa para Drywall',
     image: 'assets/images/massa_para_drywall.png',
     description: 'Pronta para uso, ideal para nivelar e dar acabamento em juntas e superfícies de Drywall.',
-    category: 'Massa'
+    category: Category.jointCompound.name
   ),
 ];
+
+final List<Product> screwsList = [
+  ...allProducts.where((value) =>
+      value.category == Category.screws.name
+)];
+
+final List<Product> tapesList = [
+  ...allProducts.where((value) =>
+      value.category == Category.tape.name
+)];
+
+final List<Product> sheetsList = [
+  ...allProducts.where((value) =>
+      value.category == Category.sheet.name
+)];
+
+final List<Product> metalFramingList = [
+  ...allProducts.where((value) =>
+    value.category == Category.metalFraming.name
+)];
+final List<Product> jointCompoundsList = [
+  ...allProducts.where((value) =>
+  value.category == Category.metalFraming.name
+  )];
--- forte_distribuidora/lib/domain/data/models/product.dart
+++ forte_distribuidora/lib/domain/data/models/product.dart
@@ -1,8 +1,21 @@
 class Product {
-  Product({required this.name, required this.image, required this.description, required this.category});
+  Product({
+    required this.name,
+    required this.image,
+    required this.description,
+    required this.category,
+  });
 
   final String name;
   final String image;
   final String description;
   final String category;
+}
+
+enum Category {
+  sheet,
+  metalFraming,
+  tape,
+  screws,
+  jointCompound
 }
