05d62e86cf583dc9d5bab284e9f2d89fa06e14a4
Jonathan <dos.santos.2k23@gmail.com>
2025-06-16 20:38:49 -0300

    Feature: Implement products_by_category.dart

---
+++ forte_distribuidora/lib/screens/widgets/product/products_section.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/cupertino.dart';
+import 'package:forte_distribuidora/screens/widgets/product/test.dart';
+import 'package:forte_distribuidora/screens/widgets/product/top_picks.dart';
+import 'package:forte_distribuidora/screens/widgets/home/value_proposition.dart';
+import 'package:forte_distribuidora/screens/widgets/product/products_by_category.dart';
+
+import '../../../utils/constants.dart';
+import '../../../utils/responsive.dart';
+
+class ProductSection extends StatelessWidget {
+  const  ProductSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          constraints: const BoxConstraints(maxWidth: kMaxWidth),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // const SizedBox(height: 20),
+              // Container(
+              //   padding: const EdgeInsets.symmetric(vertical: 5),
+              //   margin: const EdgeInsets.symmetric(horizontal: 10),
+              //   decoration: const BoxDecoration(
+              //     border: Border(
+              //       bottom: BorderSide(color: kSecondaryColor, width: 3),
+              //     ),
+              //   ),
+              //   child: const Text(
+              //     "Brands",
+              //     style: TextStyle(fontSize: 23, fontWeight: FontWeight.w600),
+              //   ),
+              // ),
+
+              const SizedBox(height: 20),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: kSecondaryColor, width: 3),
+                  ),
+                ),
+                child: const Text(
+                  "Em Destaque",
+                  style: TextStyle(fontSize: 23, fontWeight: FontWeight.w600),
+                ),
+              ),
+              const SizedBox(height: 20),
+              if (Responsive.isDesktop(context)) const TopPicksGrid(),
+              if (!Responsive.isDesktop(context)) const TopPicksGridMobile(),
+
+              //now we create trending Product model
+              const SizedBox(height: 20),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: kSecondaryColor, width: 3),
+                  ),
+                ),
+                child: const Text(
+                  "Nossos Produtos",
+                  style: TextStyle(fontSize: 23, fontWeight: FontWeight.w600),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const ProductsByCategory(),
+              const SizedBox(height: 20),
+              const ValueProposition(),
+              // const SizedBox(height: 20),
+        //       Container(
+        //         padding: const EdgeInsets.symmetric(vertical: 5),
+        //         margin: const EdgeInsets.symmetric(horizontal: 10),
+        //         decoration: const BoxDecoration(
+        //           border: Border(
+        //             bottom: BorderSide(color: kSecondaryColor, width: 3),
+        //           ),
+        //         ),
+        //         child: const Text(
+        //           "Author",
+        //           style: TextStyle(fontSize: 23, fontWeight: FontWeight.w600),
+        //         ),
+        //       ),
+        //       const SizedBox(height: 20),
+        //       const Padding(
+        //         padding: EdgeInsets.symmetric(horizontal: 10),
+        //         child: Author(),
+        //       ),
+        //
+        //       // now add all brands data
+        //       //first of all we create model
+        //       const SizedBox(height: 100),
+        //       //bottom Navigation menu
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
