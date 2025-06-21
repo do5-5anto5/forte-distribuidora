6baef85ac123ac25a6f66ed46fdb504542b27e62
Jonathan <dos.santos.2k23@gmail.com>
2025-06-21 17:45:14 -0300

    Feature: Change isHover state when click (tap) on Product card

--- forte_distribuidora/lib/screens/widgets/product/product_card.dart
+++ forte_distribuidora/lib/screens/widgets/product/product_card.dart
@@ -8,6 +8,7 @@ import '../../../utils/constants.dart';
 class ProductContentShow extends StatefulWidget {
   final Product product;
   final VoidCallback press;
+
   const ProductContentShow({
     super.key,
     required this.product,
@@ -20,95 +21,103 @@ class ProductContentShow extends StatefulWidget {
 
 class _ProductContentShowState extends State<ProductContentShow> {
   bool isHover = false;
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
-        onTap: widget.press,
-        onHover: (value) {
-          setState(() {
-            isHover = value;
-          });
-        },
-        child: AnimatedContainer(
-          duration: kDefaultDuration,
-          width: 300,
-          height: 250,
-          decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: kgreyColor, width: 3),
-              boxShadow: [if (isHover) kDefaultShadow]),
-          child: Stack(
-            fit: StackFit.passthrough,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    Image.asset(
-                      widget.product.image,
-                      height: 150,
-                      width: 150,
-                      fit: BoxFit.contain,
+      onTap: () {
+        setState(() {
+          isHover == false ? true : false;
+        });
+        widget.press;
+      },
+      onHover: (value) {
+        setState(() {
+          isHover = value;
+        });
+      },
+      child: AnimatedContainer(
+        duration: kDefaultDuration,
+        width: 300,
+        height: 250,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(color: kgreyColor, width: 3),
+          boxShadow: [if (isHover) kDefaultShadow],
+        ),
+        child: Stack(
+          fit: StackFit.passthrough,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Image.asset(
+                    widget.product.image,
+                    height: 150,
+                    width: 150,
+                    fit: BoxFit.contain,
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    widget.product.name,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
                     ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      widget.product.name,
-                      style:
-                      const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                    ),
-                    // const SizedBox(
-                    //   height: 10,
-                    // ),
-                    // Text(
-                    //   "\$${widget.product.price}",
-                    //   style: const TextStyle(
-                    //     fontSize: 16,
-                    //   ),
-                    // ),
-                  ],
-                ),
+                  ),
+                  // const SizedBox(
+                  //   height: 10,
+                  // ),
+                  // Text(
+                  //   "\$${widget.product.price}",
+                  //   style: const TextStyle(
+                  //     fontSize: 16,
+                  //   ),
+                  // ),
+                ],
               ),
-              isHover
-                  ? Container(
-                color: Colors.black12,
-                /////////// OPTIONAL BUTTONS
-                // child: Column(
-                //   mainAxisAlignment: MainAxisAlignment.center,
-                //   children: [
-                //     MaterialButton(
-                //       color: kSecondaryColor,
-                //       shape: RoundedRectangleBorder(
-                //           borderRadius: BorderRadius.circular(50)),
-                //       height: 40,
-                //       onPressed: () {},
-                //       child: const Text(
-                //         "Quick View",
-                //         style: TextStyle(color: kWhiteColor),
-                //       ),
-                //     ),
-                //     const SizedBox(
-                //       height: 20,
-                //     ),
-                //     MaterialButton(
-                //       shape: RoundedRectangleBorder(
-                //           borderRadius: BorderRadius.circular(50)),
-                //       color: kSecondaryColor,
-                //       height: 40,
-                //       onPressed: () {},
-                //       child: const Text(
-                //         "Shop Now",
-                //         style: TextStyle(color: kWhiteColor),
-                //       ),
-                //     ),
-                //   ],
-                // ),
-              )
-                  : Container(),
-            ],
-          ),
-        ));
+            ),
+            isHover
+                ? Container(
+                  color: Colors.black12,
+                  /////////// OPTIONAL BUTTONS
+                  // child: Column(
+                  //   mainAxisAlignment: MainAxisAlignment.center,
+                  //   children: [
+                  //     MaterialButton(
+                  //       color: kSecondaryColor,
+                  //       shape: RoundedRectangleBorder(
+                  //           borderRadius: BorderRadius.circular(50)),
+                  //       height: 40,
+                  //       onPressed: () {},
+                  //       child: const Text(
+                  //         "Quick View",
+                  //         style: TextStyle(color: kWhiteColor),
+                  //       ),
+                  //     ),
+                  //     const SizedBox(
+                  //       height: 20,
+                  //     ),
+                  //     MaterialButton(
+                  //       shape: RoundedRectangleBorder(
+                  //           borderRadius: BorderRadius.circular(50)),
+                  //       color: kSecondaryColor,
+                  //       height: 40,
+                  //       onPressed: () {},
+                  //       child: const Text(
+                  //         "Shop Now",
+                  //         style: TextStyle(color: kWhiteColor),
+                  //       ),
+                  //     ),
+                  //   ],
+                  // ),
+                )
+                : Container(),
+          ],
+        ),
+      ),
+    );
   }
 }
 
@@ -117,9 +126,7 @@ class _ProductContentShowState extends State<ProductContentShow> {
 class ProductCard extends StatelessWidget {
   final List<Product> prodList;
 
-  const ProductCard({
-    super.key, required this.prodList,
-  });
+  const ProductCard({super.key, required this.prodList});
 
   @override
   Widget build(BuildContext context) {
@@ -131,10 +138,9 @@ class ProductCard extends StatelessWidget {
         physics: const ScrollPhysics(),
         crossAxisCount: 4,
         itemCount: prodList.length,
-        itemBuilder: (BuildContext context, int index) => ProductContentShow(
-          press: () {},
-          product: prodList[index],
-        ),
+        itemBuilder:
+            (BuildContext context, int index) =>
+                ProductContentShow(press: () {}, product: prodList[index]),
         staggeredTileBuilder: (int index) => const StaggeredTile.fit(1),
         mainAxisSpacing: 10.0,
         crossAxisSpacing: 10.0,
@@ -146,9 +152,7 @@ class ProductCard extends StatelessWidget {
 class ProductCardMobile extends StatelessWidget {
   final List<Product> prodList;
 
-  const ProductCardMobile({
-    super.key, required this.prodList,
-  });
+  const ProductCardMobile({super.key, required this.prodList});
 
   @override
   Widget build(BuildContext context) {
@@ -158,10 +162,9 @@ class ProductCardMobile extends StatelessWidget {
       physics: const ScrollPhysics(),
       crossAxisCount: 2,
       itemCount: prodList.length,
-      itemBuilder: (BuildContext context, int index) => ProductContentShow(
-        press: () {},
-        product: prodList[index],
-      ),
+      itemBuilder:
+          (BuildContext context, int index) =>
+              ProductContentShow(press: () {}, product: prodList[index]),
       staggeredTileBuilder: (int index) => const StaggeredTile.fit(1),
       mainAxisSpacing: 10.0,
       crossAxisSpacing: 10.0,
