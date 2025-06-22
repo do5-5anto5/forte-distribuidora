880eebf19252dfafc16eacc8750c6b21e52d5ea2
Jonathan <dos.santos.2k23@gmail.com>
2025-06-22 17:57:50 -0300

    Feature: Initialize Firebase with platform-specific options in main()

--- forte_distribuidora/lib/main.dart
+++ forte_distribuidora/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:forte_distribuidora/firebase_options.dart';
 import 'package:forte_distribuidora/screens/home/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
